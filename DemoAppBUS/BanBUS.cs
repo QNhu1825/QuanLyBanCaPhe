386c9c780e293d00b676256709c395fc84a771d9
QNhu1825 <0306231320@caothang.edu.vn>
2025-10-15 22:01:33 +0700

    Add BUS

---
+++ DemoAppBUS/BanBUS.cs
@@ -0,0 +1,25 @@
+﻿using DemoAppDTO;
+using DemoAppDAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoAppBUS
+{
+    public class BanBUS
+    {
+       
+        private BanDAO ban=new BanDAO();
+        public List<BanDTO> LoadDSBan()
+        {
+            return ban.LoadDSBan();
+        }
+        public void ChuyenBan(char idban, char idbanmoi)
+        {
+            ban.ChuyenBan(idban, idbanmoi);
+        }
+
+    }
+}
